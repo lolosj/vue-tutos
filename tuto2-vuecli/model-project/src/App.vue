ec4844662223055e1e2adc1f508028f37a3118a2
lolo <adcomere@gmail.com>
2021-08-24 03:06:09 +0700

    tutos

---
+++ tuto2-vuecli/model-project/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+ <div id="containerheader">
+  <img alt="Truffle Pig !" src="./assets/logo2.jpg">
+  <h1>{{ title }}</h1>
+</div>   
+
+<input type="text" ref="inputlolo">
+<button @click="clickButton()">click here</button>
+<Modal :titre="titreLolo" :lolo="arrayVar" theme="sale"/>
+</template> 
+
+
+<script>
+
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'Pigboard Frontend',
+      titreLolo: 'titre ici',
+      arrayVar: [1, 5, 78],
+    }; 
+  },
+  methods: {
+    clickButton() {
+      console.log(this.$refs.inputlolo.value);
+      this.$refs.inputlolo.classList.add('active');
+      this.$refs.inputlolo.focus();
+
+    }
+  },
+}
+</script>
+
+<style>
+#containerheader {
+  max-width: 444px;
+  margin: auto;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: auto;
+}
+
+h1 {
+  border-bottom: 1px solid;
+  display: block;
+  padding-bottom: 10px;
+  color: black !important;
+}
+</style>
---
+++ tuto2-vuecli/model-project/src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1> {{ titre }} </h1>
+            https://www.youtube.com/watch?v=KM1U6DqZf8M&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=5 15m41
+            <p v-for="a in lolo"> {{ a }} </p>
+        </div>
+    </div>
+</template> 
+
+<script>
+export default {
+    props: ['titre', 'lolo', 'theme']
+}
+
+</script>
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 400px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%; 
+    height: 100%;
+    margin: auto;
+} 
+
+.modal h1 {
+ border-bottom: none;
+  display: block;
+  padding-bottom: 10px;
+  color: #03cfb4 !important;
+}
+
+</style>
