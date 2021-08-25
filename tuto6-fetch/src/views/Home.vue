b2af80927a1ae239c5c87cb652828d46dc58be43
lolo <adcomere@gmail.com>
2021-08-25 23:51:14 +0700

    tuto6

---
+++ tuto6-fetch/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1> Homepage </h1>
+    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quas explicabo fugiat veritatis beatae quo adipisci? Ex quod tempore in doloremque veritatis consequuntur! Quam est, vero provident quas quasi neque, sit, nemo fugiat quaerat id earum amet molestiae excepturi adipisci eveniet perspiciatis nostrum quia ipsam atque. Iste odio sapiente autem quam ipsa explicabo id et sequi, beatae dolorem officia deleniti at harum unde quis optio aliquid alias perspiciatis, magni modi ab rem! Facilis harum ad sed sint dolorem voluptatem placeat itaque libero atque eum, hic expedita cum officia doloremque minima! Impedit quas deleniti tempora animi aperiam harum unde et architecto.</p>
+    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quas explicabo fugiat veritatis beatae quo adipisci? Ex quod tempore in doloremque veritatis consequuntur! Quam est, vero provident quas quasi neque, sit, nemo fugiat quaerat id earum amet molestiae excepturi adipisci eveniet perspiciatis nostrum quia ipsam atque. Iste odio sapiente autem quam ipsa explicabo id et sequi, beatae dolorem officia deleniti at harum unde quis optio aliquid alias perspiciatis, magni modi ab rem! Facilis harum ad sed sint dolorem voluptatem placeat itaque libero atque eum, hic expedita cum officia doloremque minima! Impedit quas deleniti tempora animi aperiam harum unde et architecto.</p>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
---
+++ tuto6-fetch/src/views/jobs/JobDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1> Job Details Page </h1>
+  <div v-if="job">
+    <p> {{ job.title }} </p>
+    <p> {{ job.details }} </p>
+  </div>
+  <div v-else>
+  <p> loading ... </p>
+  </div>
+</template>
+
+<script>
+export default {
+    // data() {
+    //     return {
+    //         jobId: this.$route.params.id
+    //     }
+    // },
+    data() {
+      return {
+        job: null
+      }
+    },
+    props: ['id'],
+    mounted() {
+      fetch('http://localhost:3000/jobs/' + this.id)
+        .then( res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message));    
+    }
+}
+</script>
+
+<style>
+
+</style>
